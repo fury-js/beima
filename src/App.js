1d7dbfb46b247627f349e88e71501746d6141e54
davien21 <chidiebereekennia@gmail.com>
2021-11-20 04:56:26 +0100

    --feat: add the app provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import AppRouter from "./appRouter";
+import { AppProvider } from "./contexts/appContext";
 
 function App() {
   return (
     <div className="App">
       <Router basename={"/"}>
-        <AppRouter />
+        <AppProvider>
+          <AppRouter />
+        </AppProvider>
       </Router>
     </div>
   );
