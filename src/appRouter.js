02e474d72f39c57b88ba4d0b25167d40c758cf9d
davien21 <chidiebereekennia@gmail.com>
2021-11-20 04:56:03 +0100

    --feat: add a dashboard home page

--- src/appRouter.js
+++ src/appRouter.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { Redirect, Route, Switch, useLocation } from "react-router-dom";
 import { Header } from "./components";
 
-import { LandingPage } from "./pages";
+import { DashboardHome, LandingPage } from "./pages";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,9 +19,9 @@ const AppRouter = () => {
   return (
     <>
       <Header />
-
       <ScrollToTop />
       <Switch>
+        <Route path="/dashboard" render={(props) => <DashboardHome />} />
         <Route path="/" render={(props) => <LandingPage />} />
         <Route path="*" render={(props) => <Redirect to="/" />} />
       </Switch>
