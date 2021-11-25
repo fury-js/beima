045101f10838bbd8a32c3f71ef7209bac352e961
fury-js <chukkyc582@gmail.com>
2021-11-25 12:36:06 +0100

    modified contract abis

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-const BeimaContractAddress = '0x36F3Ac5140EF8c696826FeE7F1A97374838001e1';
+const BeimaContractAddress = '0x130dDd8D3f9a4ceD982ae17890e9592DF4B64D96';
+const BeimaContractAddressMainNet = '0x36F3Ac5140EF8c696826FeE7F1A97374838001e1';
+
 
 export {
     BeimaContractAddress
